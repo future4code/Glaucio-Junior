06cd0154a54aa21fb01faffbe9cc15faec323394
Glaucio-Junior <glaucio_oliveira21@outlook.com>
2021-09-10 18:54:11 -0300

    commit_operadores

---
+++ quinzena1/quinzena1/variaveis/Aula_05/index.js
@@ -0,0 +1,75 @@
+// Soma 
+// let numero1 = 3
+// let numero2 = 4
+
+// let resultadoDaSoma  = numero1 + numero2 
+// console.log ("O resultado é:" ,resultadoDaSoma)
+
+// Multiplicar e dividir 
+
+// let numero3 = 3 
+// let numero4 = 5 
+// let numero5 = 2 
+
+// let resultadoDaOperaçao = (numero3 * numero4) / numero5 
+// console.log("O resultado final é:", resultadoDaOperaçao)
+
+// 
+// let resultadoDaSubtraçao = (5-4)*-1 
+// console.log("O resultado é: ", resultadoDaSubtraçao)
+
+// criar dois numeros 
+
+// let numeroA = 10
+// let numeroB = 50    
+
+// let verificaçao = (numeroA === numeroB)
+// console.log(verificaçao) // false 
+// console.log(numeroA !== numeroB) // true 
+// console.log(numeroA > numeroB) // false
+// console.log(numeroA < numeroB)//true 
+
+// booleano
+
+// const a = true 
+// const b = false
+// const c = true 
+
+// console.log (a && b )
+// console.log (a && c)
+// console.log (b && c)
+// console.log (a && b && c)
+
+
+// exercicio 5 
+
+// const nome = prompt ("Informe seu nome por favor:")
+// const anoNascimento = Number(prompt("Informe o ano em que nasceu:"))
+// const anoAtual = Number(prompt("Informe o ano atual:"))
+// const idade = anoAtual - anoNascimento
+
+// const verificaçao = (idade >= 18)
+// const soma = 2050 - anoNascimento
+
+// console.log("Olá," ,nome)
+// console.log("Ano de nascimento" ,anoNascimento)
+// console.log("Ano atual:" ,anoAtual)
+// console.log("Sua idade é:" ,idade)
+// console.log("Você é maior de idade:" ,verificaçao)
+// console.log("Você terá em 2050:" ,soma , "anos")
+
+// meu primeiro exercicio 
+// let nome = prompt ("Informe seu nome por favor:")
+
+// let idade = prompt("Informe sua idade por favor:")
+
+// let soma = 2050 - 1997
+
+// const verificaçao = (idade > 18)
+
+// console.log("Olá,",nome)
+// console.log("Idade:",idade)
+// console.log("É maior de idade",verificaçao)
+// console.log("idade em 2050:" ,soma)
+
+
